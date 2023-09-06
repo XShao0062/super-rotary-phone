d6a09d411eb3821be66b76027188da35f4be12bb
shaoxi <shaoxi@shao.ac.cn>
2023-09-06 22:04:54 +0800

    add test on function user_reminder

---
+++ tests/tests_user_reminder.py
@@ -0,0 +1,16 @@
+def test_module_import():
+    try:
+        import coord_cal
+    except Exception as e:
+        raise AssertionError("Failed to import mymodule")
+    return
+
+
+
+def test_user_reminder():
+    try:
+        import coord_cal
+    except Exception as e:
+        raise AssertionError("Failed to import mymodule")
+    return
+
